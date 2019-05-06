0e269af7c7394101f787eba47045bead817c19bb
Ellysha Chavez <ellyshachavez@gmail.com>
2019-05-05 21:29:49 -0700

    Updates

---
+++ precache-manifest.0f27d8f36edcaee8b52c52fc006c0523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaeefc913a95130b41fedccc2fadee9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "faef812b25b5c3b5c0cd",
+    "url": "/Clicky-Game/static/css/main.5715fd75.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "faef812b25b5c3b5c0cd",
+    "url": "/Clicky-Game/static/js/main.0a9b5785.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
